22538227529a38dec1aa9e484f57869ceff5499d
amanleykj <118389631+amanleykj@users.noreply.github.com>
2023-10-14 14:14:33 +0900

    developing alt site

--- src/utils/index.js
+++ src/utils/index.js
@@ -39,6 +39,10 @@ export const adminNavOptions = [
     },
 ];
 
+export const styles = {
+    button : 'mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'
+};
+
 export const registrationFormControls = [
     {
     id: "name",
